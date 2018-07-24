37ae35bea24994154f4fa6cecf8d315c0d3efddd
Thomas Robitaille <thomas.robitaille@gmail.com>
2018-07-24 09:55:07 +0100

    Find PR/issue numbers in changelog

--- validate.py
+++ validate.py
@@ -6,23 +6,47 @@ import docutils.parsers.rst
 import docutils.utils
 
 VERSION_PATTERN = re.compile('^v?[0-9\.]+ \([\w\-]+\)')
+BLOCK_PATTERN = re.compile('\[#.+\]', flags=re.DOTALL)
+ISSUE_PATTERN = re.compile('#[0-9]+')
 
 
-class ChangelogParser:
+def find_prs_in_entry(content):
+    issue_numbers = []
+    for block in BLOCK_PATTERN.finditer(content):
+        block_start, block_end = block.start(), block.end()
+        block = content[block_start:block_end]
+        for m in ISSUE_PATTERN.finditer(block):
+            start, end = m.start(), m.end()
+            issue_numbers.append(int(block[start:end][1:]))
+    return issue_numbers
+
+
+class BulletItemVisitor(docutils.nodes.NodeVisitor):
+
+    def __init__(self, document, warn):
+        super(BulletItemVisitor, self).__init__(document)
+        self.warn = warn
+        self.reset_bullet_items()
+
+    def reset_bullet_items(self):
+        self.bullet_items = []
+
+    def dispatch_visit(self, node):
+        if isinstance(node, docutils.nodes.list_item):
+            self.bullet_items.append(node)
+            raise docutils.nodes.SkipChildren
+        elif isinstance(node, (docutils.nodes.title, docutils.nodes.system_message)):
+            raise docutils.nodes.SkipChildren
+        elif isinstance(node, (docutils.nodes.section, docutils.nodes.bullet_list)):
+            pass
+        else:
+            self.warn('Unexpected content: {0}'.format(node.astext()))
+
+
+class AstropyChangelog:
 
     def warn(self, message):
         print('WARNING:', message)
-
-    def validate(self, filename):
-
-        document = self.parse(filename)
-
-        # At the top level, the document should just include sections whose name is
-        # a version number followed by a release date.
-
-        for section in document:
-            title = section.attributes['names'][0]
-            self._validate_version(title)
 
     def _validate_version(self, string):
         if VERSION_PATTERN.match(string) is None:
@@ -34,8 +58,14 @@ class ChangelogParser:
         return data
 
     def parse(self, filename):
+
+        # Open file and read contents
+
         with open(filename) as f:
             text = f.read()
+
+        # Parse as rst
+
         parser = docutils.parsers.rst.Parser()
         components = (docutils.parsers.rst.Parser,)
         settings = docutils.frontend.OptionParser(components=components).get_default_values()
@@ -43,6 +73,35 @@ class ChangelogParser:
         document.reporter.stream = None
         document.reporter.attach_observer(self._parse_observer)
         parser.parse(text, document)
+
+        # At the top level, the document should just include sections whose name is
+        # a version number followed by a release date.
+
+        visitor = BulletItemVisitor(document, self.warn)
+
+        self._issues_by_version = {}
+
+        for section in document:
+
+            title = section.attributes['names'][0]
+            self._validate_version(title)
+
+            # Inside each version section there may either directly be a list of
+            # entries, or more nested sections. We use a visitor class to search
+            # for all the bullet point entries, and in the process we make sure
+            # that there are only section titles and bullet point entries in the
+            # section
+
+            visitor.reset_bullet_items()
+            section.walk(visitor)
+
+            # Go over the bullet point items and find the PR numbers
+            issues = []
+            for item in visitor.bullet_items:
+                issues.extend(find_prs_in_entry(item.astext()))
+
+        self.document = document
+
         return document
 
 
